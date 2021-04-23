c144aa34e621557f00c359f550104f0f67037d3f
Wilson <wlyshen@gmail.com>
2021-04-23 18:11:24 +0800

    changed app test text

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/there222113/i);
+  const linkElement = screen.getByText(/ I was changed on the feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
 
